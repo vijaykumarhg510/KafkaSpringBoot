012904c03e21bccb36f3f3b41646b95f570fce94
vijaykumarhg510 <Sucivi@1>
2023-06-09 19:08:34 +0530

    docker-compose file to run kafka through docker

--- Prod/src/main/java/com/example/Prod/configuration/TopicCreation.java
+++ Prod/src/main/java/com/example/Prod/configuration/TopicCreation.java
@@ -3,7 +3,6 @@ package com.example.Prod.configuration;
 
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
